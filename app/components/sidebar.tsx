92bb46779dfeaa474f3d5576b9830000b20f3933
guo xianer <1106672482@qq.com>
2023-08-02 06:08:16 +0000

    修改标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,7 +118,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT
+          MindTalk
         </div>
         <div className={styles["sidebar-sub-title"]}>100nh.</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
